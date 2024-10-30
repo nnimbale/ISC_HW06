8e2e0a70c165cede15993702e99a65d5ae38aa66
Naman Nimbale <nnimbale@iu.edu>
2024-10-30 12:08:50 -0400

    speeding up LLR

--- llr_functions.R
+++ llr_functions.R
@@ -5,9 +5,11 @@ llr = function(x, y, z, omega) {
 
 
 make_weight_matrix <- function(z, x, omega) {
-  weights <- exp(-((x - z)^2) / (2 * omega^2))
-  W <- diag(weights)
-  return(W)
+  # weights <- exp(-((x - z)^2) / (2 * omega^2))
+  # W <- diag(weights)
+  # Calculate weights without constructing a full diagonal matrix
+  exp(-((x - z)^2) / (2 * omega^2))
+  # return(W)
 }
 
 make_predictor_matrix <- function(x) {
@@ -19,7 +21,16 @@ make_predictor_matrix <- function(x) {
 compute_f_hat = function(z, x, y, omega) {
   Wz = make_weight_matrix(z, x, omega)
   X = make_predictor_matrix(x)
-  f_hat = c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
+  
+  # Apply the weights directly to each row of X and y using apply
+  weighted_X <- apply(X, 2, function(col) col * Wz)
+  weighted_y <- y * Wz
+  
+  # Compute f_hat using the modified weighted_X and weighted_y
+  f_hat <- c(1, z) %*% solve(t(X) %*% weighted_X) %*% (t(weighted_X) %*% weighted_y)
   return(f_hat)
+  
+  # f_hat = c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
+  # return(f_hat)
 }
 
