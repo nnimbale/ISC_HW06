5c67b463f1f8eb9b48af34f7c1ac354bee5bad0a
Naman Nimbale <nnimbale@iu.edu>
2024-10-30 19:13:57 -0400

    speeding up LLR 2

--- llr_functions.R
+++ llr_functions.R
@@ -5,10 +5,14 @@ llr = function(x, y, z, omega) {
 
 
 make_weight_matrix <- function(z, x, omega) {
+  
+  #normal matrix calc
   # weights <- exp(-((x - z)^2) / (2 * omega^2))
   # W <- diag(weights)
+  
   # Calculate weights without constructing a full diagonal matrix
   exp(-((x - z)^2) / (2 * omega^2))
+  
   # return(W)
 }
 
@@ -22,13 +26,28 @@ compute_f_hat = function(z, x, y, omega) {
   Wz = make_weight_matrix(z, x, omega)
   X = make_predictor_matrix(x)
   
-  # Apply the weights directly to each row of X and y using apply
-  weighted_X <- apply(X, 2, function(col) col * Wz)
+  ####------ Speed-test-2 ------####
+  # Use sweep to apply Wz to each column of X
+  weighted_X <- sweep(X, 1, Wz, `*`)
+  
+  # Vectorized multiplication of y by Wz
   weighted_y <- y * Wz
   
-  # Compute f_hat using the modified weighted_X and weighted_y
+  # Calculate f_hat using the weighted_X and weighted_y
   f_hat <- c(1, z) %*% solve(t(X) %*% weighted_X) %*% (t(weighted_X) %*% weighted_y)
   return(f_hat)
+  
+  
+  ####------ Speed-test-1 ------####
+  # Apply the weights directly to each row of X and y using apply
+  #weighted_X <- apply(X, 2, function(col) col * Wz)
+  #weighted_y <- y * Wz
+  
+  
+  ####------ Normal Mat Mul ------####
+  # Compute f_hat using the modified weighted_X and weighted_y
+  #f_hat <- c(1, z) %*% solve(t(X) %*% weighted_X) %*% (t(weighted_X) %*% weighted_y)
+  #return(f_hat)
   
   # f_hat = c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
   # return(f_hat)
